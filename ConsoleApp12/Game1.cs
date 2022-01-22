5ee47875ca49889e6760414d8a3f6e15c14de8f7
SameplayerDE <asameplayer@gmail.com>
2022-01-22 11:58:41 +0100

    Bezier Curve

--- ConsoleApp12/Game1.cs
+++ ConsoleApp12/Game1.cs
@@ -11,14 +11,15 @@ namespace ConsoleApp12
     {
         private GraphicsDeviceManager _graphics;
 
-        private Vector2 _a, _b, _c, _d;
-        private Vector2 _offsetA, _offsetD;
+        private Vector2 _a, _b, _c, _d, _e, _f, _g;
+        private Vector2 _offsetA, _offsetD, _offsetG;
         private float _x;
 
-        private bool _pickA, _pickB, _pickC, _pickD;
-        private float _radiusA, _radiusB, _radiusC, _radiusD;
+        private bool _pickA, _pickB, _pickC, _pickD, _pickE, _pickF, _pickG;
+        private float _radiusA, _radiusB, _radiusC, _radiusD, _radiusE, _radiusF, _radiusG;
 
-        private List<Vector2> _curve;
+        private List<Vector2> _curveA;
+        private List<Vector2> _curveB;
 
         private MouseState _currMouseState;
         private MouseState _prevMouseState;
@@ -38,7 +39,7 @@ namespace ConsoleApp12
             var totalTime = (float)gameTime.TotalGameTime.TotalSeconds;
 
             var keyboardState = Keyboard.GetState();
-            
+
             _prevMouseState = _currMouseState;
             _currMouseState = Mouse.GetState();
 
@@ -59,43 +60,50 @@ namespace ConsoleApp12
             var distanceB = Vector2.Distance(_b, mousePosition);
             var distanceC = Vector2.Distance(_c, mousePosition);
             var distanceD = Vector2.Distance(_d, mousePosition);
+            var distanceE = Vector2.Distance(_e, mousePosition);
+            var distanceF = Vector2.Distance(_f, mousePosition);
+            var distanceG = Vector2.Distance(_g, mousePosition);
 
             if (_currMouseState.LeftButton == ButtonState.Pressed)
             {
-                if (!_pickA && !_pickB && !_pickC && !_pickD)
+                if (!_pickA && !_pickB && !_pickC && !_pickD && !_pickE && !_pickF && !_pickG)
                 {
-                    if (distanceA < distanceB && distanceA < distanceC && distanceA < distanceD)
+                    if (distanceA < 20)
                     {
-                        if (distanceA < 20)
-                        {
-                            _pickA = true;
-                            _offsetA = _a - mousePosition;
-                        }
+                        _pickA = true;
+                        _offsetA = _a - mousePosition;
                     }
 
-                    if (distanceB < distanceA && distanceB < distanceC && distanceB < distanceD)
+                    else if (distanceB < 20)
                     {
-                        if (distanceB < 20)
-                        {
-                            _pickB = true;
-                        }
+                        _pickB = true;
                     }
 
-                    if (distanceC < distanceA && distanceC < distanceB && distanceC < distanceD)
+                    else if (distanceC < 20)
                     {
-                        if (distanceC < 20)
-                        {
-                            _pickC = true;
-                        }
+                        _pickC = true;
                     }
-                    
-                    if (distanceD < distanceA && distanceD < distanceB && distanceD < distanceC)
+
+                    else if (distanceD < 20)
                     {
-                        if (distanceD < 20)
-                        {
-                            _pickD = true;
-                            _offsetD = _d - mousePosition;
-                        }
+                        _pickD = true;
+                        _offsetD = _d - mousePosition;
+                    }
+
+                    else if (distanceE < 20)
+                    {
+                        _pickE = true;
+                    }
+
+                    else if (distanceF < 20)
+                    {
+                        _pickF = true;
+                    }
+
+                    else if (distanceG < 20)
+                    {
+                        _pickG = true;
+                        _offsetG = _g - mousePosition;
                     }
                 }
             }
@@ -105,25 +113,17 @@ namespace ConsoleApp12
                 _pickB = false;
                 _pickC = false;
                 _pickD = false;
-                if (distanceA < distanceB && distanceA < distanceC)
-                {
-                    _radiusA = distanceA < 20 ? 10f : 5f;
-                }
+                _pickE = false;
+                _pickF = false;
+                _pickG = false;
 
-                if (distanceB < distanceA && distanceB < distanceC)
-                {
-                    _radiusB = distanceB < 20 ? 10f : 5f;
-                }
-
-                if (distanceC < distanceA && distanceC < distanceB)
-                {
-                    _radiusC = distanceC < 20 ? 10f : 5f;
-                }
-                
-                if (distanceD < distanceA && distanceD < distanceB && distanceD < distanceC)
-                {
-                    _radiusD = distanceD < 20 ? 10f : 5f;
-                }
+                _radiusA = distanceA < 20 ? 10f : 5f;
+                _radiusB = distanceB < 20 ? 10f : 5f;
+                _radiusC = distanceC < 20 ? 10f : 5f;
+                _radiusD = distanceD < 20 ? 10f : 5f;
+                _radiusE = distanceE < 20 ? 10f : 5f;
+                _radiusF = distanceF < 20 ? 10f : 5f;
+                _radiusG = distanceG < 20 ? 10f : 5f;
             }
 
             if (_pickA)
@@ -141,14 +141,31 @@ namespace ConsoleApp12
             {
                 _c = mousePosition;
             }
-            
+
             if (_pickD)
             {
                 _d = mousePosition + _offsetD;
                 _c += mouseDelta;
+                _e += mouseDelta;
             }
 
-            if (_pickA || _pickB || _pickC || _pickD)
+            if (_pickE)
+            {
+                _e = mousePosition;
+            }
+
+            if (_pickF)
+            {
+                _f = mousePosition;
+            }
+
+            if (_pickG)
+            {
+                _g = mousePosition + _offsetG;
+                _f += mouseDelta;
+            }
+
+            if (_pickA || _pickB || _pickC || _pickD || _pickE || _pickF || _pickG)
             {
                 GenerateCurve(25);
             }
@@ -158,7 +175,9 @@ namespace ConsoleApp12
 
         private void GenerateCurve(int segments)
         {
-            _curve.Clear();
+            _curveA.Clear();
+            _curveB.Clear();
+            
             _x = 0;
 
             var change = 100 / segments;
@@ -172,8 +191,13 @@ namespace ConsoleApp12
                 var (xb, yb) = _b;
                 var (xc, yc) = _c;
                 var (xd, yd) = _d;
+                var (xe, ye) = _e;
+                var (xf, yf) = _f;
+                var (xg, yg) = _g;
 
-                var (xcurve, ycurve) = Bezier.CubicBezier(
+                float xcurve, ycurve;
+                    
+                (xcurve, ycurve) = Bezier.CubicBezier(
                     _x,
                     xa, ya,
                     xb, yb,
@@ -181,7 +205,17 @@ namespace ConsoleApp12
                     xd, yd
                 );
 
-                _curve.Add(new Vector2(xcurve, ycurve));
+                _curveA.Add(new Vector2(xcurve, ycurve));
+                
+                (xcurve, ycurve) = Bezier.CubicBezier(
+                    _x,
+                    xd, yd,
+                    xe, ye,
+                    xf, yf,
+                    xg, yg
+                );
+
+                _curveB.Add(new Vector2(xcurve, ycurve));
 
                 x += change;
             }
@@ -231,12 +265,33 @@ namespace ConsoleApp12
                 _c,
                 _radiusC
             );
-            
-            PrimitiveRenderer.DrawCircleF(
+
+            PrimitiveRenderer.DrawCircleH(
                 null,
                 Color.Red,
                 _d,
                 _radiusD
+            );
+            
+            PrimitiveRenderer.DrawCircleF(
+                null,
+                Color.Blue,
+                _e,
+                _radiusE
+            );
+            
+            PrimitiveRenderer.DrawCircleF(
+                null,
+                Color.Blue,
+                _f,
+                _radiusF
+            );
+            
+            PrimitiveRenderer.DrawCircleF(
+                null,
+                Color.Red,
+                _g,
+                _radiusG
             );
 
             PrimitiveRenderer.DrawLine(
@@ -244,11 +299,23 @@ namespace ConsoleApp12
                 Color.Blue,
                 _a, _b
             );
-            
+
             PrimitiveRenderer.DrawLine(
                 null,
                 Color.Blue,
                 _c, _d
+            );
+
+            PrimitiveRenderer.DrawLine(
+                null,
+                Color.Blue,
+                _d, _e
+            );
+
+            PrimitiveRenderer.DrawLine(
+                null,
+                Color.Blue,
+                _f, _g
             );
             
             DrawBezier();
@@ -258,10 +325,21 @@ namespace ConsoleApp12
 
         private void DrawBezier()
         {
-            for (int index = 1; index < _curve.Count; index += 1)
+            for (int index = 1; index < _curveA.Count; index += 1)
             {
-                var a = _curve[index - 1];
-                var b = _curve[index - 0];
+                var a = _curveA[index - 1];
+                var b = _curveA[index - 0];
+                PrimitiveRenderer.DrawLine(
+                    null,
+                    Color.Red,
+                    a, b
+                );
+            }
+            
+            for (int index = 1; index < _curveB.Count; index += 1)
+            {
+                var a = _curveB[index - 1];
+                var b = _curveB[index - 0];
                 PrimitiveRenderer.DrawLine(
                     null,
                     Color.Red,
@@ -274,20 +352,28 @@ namespace ConsoleApp12
         {
             PrimitiveRenderer.Initialise(GraphicsDevice);
 
-            _curve = new List<Vector2>();
+            _curveA = new List<Vector2>();
+            _curveB = new List<Vector2>();
 
-            _a = new Vector2(0, 0);
+            _a = new Vector2(-100, 100);
+            _d = new Vector2(0, 0);
+            _g = new Vector2(100, -100);
+            
             _b = new Vector2(0, 100);
             _c = new Vector2(-100, 0);
-            _d = new Vector2(-100, 100);
+            _e = new Vector2(100, 0);
+            _f = new Vector2(0, -100);
 
             _radiusA = 5f;
             _radiusB = 5f;
             _radiusC = 5f;
             _radiusD = 5f;
+            _radiusE = 5f;
+            _radiusF = 5f;
+            _radiusG = 5f;
 
             GenerateCurve(25);
-            
+
             base.Initialize();
         }
     }
